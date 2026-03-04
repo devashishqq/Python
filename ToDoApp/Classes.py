87db99363df5a7fb36f5df00c1aa1d2a5c64bd1d
ashish <dev.ashish.qq@gmail.com>
2026-03-04 15:15:15 +0530

    todo

--- ToDoApp/Classes.py
+++ ToDoApp/Classes.py
@@ -1,11 +1,18 @@
-class ToDo:
-    def __init__(self):
-        pass
+class Todo:
+    def __init__(self, name):
+        self.name = name
+        print(self.name)
+        self.d = {}
 
-    def create(self, note, time):
-        self.note = note
-        self.time = time
-        l = []
-        l.append(self.note)
-        l.append(self.time)
-        print(l)
+    def create(self, sno, note, time):
+        self.d[int(sno)] = [note, time]
+        for key,values in self.d.items():
+            print(f"{key}: {values[0]} at {values[1]}")
+
+    def delete(self, sno):
+        self.d.pop(int(sno), None)
+        print(self.d)
+
+    def showtodo(self):
+        for key, value in self.d.items():
+            print(key, value[0], value[1])
--- ToDoApp/operator.py
+++ ToDoApp/operator.py
@@ -1,3 +1,26 @@
-from Classes import *
-ToDoApp = ToDo()
+from Classes import Todo
 
+user_name = input("Enter your name: ")
+user_name = Todo(user_name)
+
+while True:
+    choice = input("1: Add  2: Remove  3: Exit → ")
+
+    if choice == "1":
+        sno = input("Serial number: ")
+        note = input("Task: ")
+        time = input("Time: ")
+        user_name.create(sno, note, time)
+
+    elif choice == "2":
+        sno = input("Serial number to delete: ")
+        user_name.delete(sno)
+
+    elif choice == "3":
+        print("Goodbye 👋")
+        break
+
+    else:
+        print("Invalid choice.")
+
+user_name.showtodo()
